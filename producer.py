60a0d304d90d6688c4f9e1afc2e2b70ce057645b
goo-gy <lkukh17@naver.com>
2025-03-17 23:23:05 +0900

    refactor : producer class 화

--- producer.py
+++ producer.py
@@ -7,28 +7,37 @@ from common.constants import CONFIG_PATH, TYPE_TOPIC
 
 CONFIG = configparser.ConfigParser()
 CONFIG.read(CONFIG_PATH)
-
 HOST = CONFIG.get('default', 'host')
 EXCHANGE_NAME = CONFIG.get('default', 'exchange_name')
 ROUTING_KEY = CONFIG.get('producer', 'routing_key')
 
-# channel
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST, port=5672))
-channel = connection.channel()
 
-# exchange
-channel.exchange_declare(exchange=EXCHANGE_NAME, exchange_type=TYPE_TOPIC)
+class Producer:
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST, port=5672))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=EXCHANGE_NAME, exchange_type=TYPE_TOPIC)
 
-# message
-while True:
-    message = input("Type message: ")
-    message_dict = {
-        'message': message
-    }
-    channel.basic_publish(
-        exchange=EXCHANGE_NAME,
-        routing_key=ROUTING_KEY,
-        body=json.dumps(message_dict),
-    )
+    def __del__(self):
+        self.connection.close()
 
-connection.close() # todo : class
+    def produce(self):
+        while True:
+            message = input("Type message: ")
+            self.__publish_message(message)
+
+    def __publish_message(self, message):
+        message_dict = {
+            'message': message
+        }
+        self.channel.basic_publish(
+            exchange=EXCHANGE_NAME,
+            routing_key=ROUTING_KEY,
+            body=json.dumps(message_dict),
+        )
+
+
+
+if __name__ == '__main__':
+    producer = Producer()
+    producer.produce()
